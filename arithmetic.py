de881241e6aaf8400feb47eaa483c6caa1e4d406
Hackbright Student <info@hackbrightacademy.com>
2015-02-01 14:15:42 -0800

    Refactored code:
    -moved float conversion to arithmetic.py
    -added dictionary of operands to arithmetic.py
    -modified calculator.py imports
    -optimized returns and control flow
    -optimized exception handling
    -added instruction message for initialization and error response
    -edited docstrings

--- arithmetic.py
+++ arithmetic.py
@@ -8,7 +8,9 @@ def multiply(num1, num2):
     return num1 * num2
 
 def divide(num1, num2):
-    return float(num1 / num2)
+    a = float(num1)
+    b = float(num2)
+    return a / b
 
 def square(num1):
     return num1 * num1
@@ -21,3 +23,14 @@ def power(num1, num2):
 
 def mod(num1, num2):
     return num1 % num2
+
+operators = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+    "square": square,
+    "cube": cube,
+    "pow": power,
+    "mod": mod
+}
--- calculator.py
+++ calculator.py
@@ -1,78 +1,93 @@
-"""
-calculator.py
-
-Using our arithmetic.py file from Exercise02, create the
-calculator program yourself in this file.
-"""
-from arithmetic import add, subtract, multiply, divide, square, cube, power, mod
+from arithmetic import operators
 
 
-# def is_digits(comp):
-#     '''takes list; breaks it down into characters; 
-#     tests if each character after the operator is a digit'''
-#     for operand in comp[1:]:
-#         for char in operand:
-#             if char not in ["0", "2", "3typ:
-#                 print "Please make sure that you are entering only numbers after the operator."
-#                 return False
-#             else:
-#                 return True
+def instructions():
+    print """
+    Instructions: 
+
+    Choose one operator from the list 
+        and provide the required number of operands:
+
+    Operator        
+           Operands 
+                 Function
+
+    +      1 2   add numbers 1 and 2
+    -      1 2   subtract number 2 from number 1
+    *      1 2   multiply numbers 1 and 2
+    /      1 2   divide number 1 by number 2
+    square 1     square the number
+    cube   1     cube the number
+    pow    1 2   raise number 1 to the power of number 2
+    mod    1 2   return the remainder of number 1 divided by number 2
+
+    q            quit
+
+    """
 
 
-def calculate(comp):
-    if len(comp) < 2:
-        print "Please provide more information"
-    elif len(comp) < 3:
-        if comp[0] == "square":
-            return square(comp[1])
-        elif comp[0] == "cube":
-            return cube(comp[1])
-        else:
-            print "Your chosen operator needs more numbers."
-    elif comp[0] == "+":
-        return add(comp[1], comp[2])
-    elif comp[0] == "-":
-        return subtract(comp[1], comp[2])
-    elif comp[0] == "*":
-        return multiply(comp[1], comp[2])
-    elif comp[0] == "/":
-        a = float(comp[1])
-        b = float(comp[2])
-        return divide(a, b)
-    elif comp[0] == "pow":
-        return power(comp[1], comp[2])
-    elif comp[0] == "mod":
-        return mod(comp[1], comp[2])
+def calculate(oper_list):
+    """ Computes digits by given operator. 
+
+    Returns None if successful, False if incorrect number of operands.
+    """
+    if len(oper_list) == 2:
+        try:
+            print operators[oper_list[0]](oper_list[1])
+        except TypeError:
+            print "Error! Please provide two operands."
+            return False
     else:
-        print "I don't understand."
+        try:
+            print operators[oper_list[0]](oper_list[1], oper_list[2])
+        except TypeError:
+            print "Error! Please provide no more than one operand."
+            return False
 
 
-def tokenize(comp):
-    '''takes in raw data and returns a list'''
-    split = comp.split()
-    operator = split[0]
-    tokens = [operator]
+def tokenize(input_string):
+    """ Converts raw data to list of [operator, digit(s)]. 
+
+    Returns list, "quit", or False (if incorrect length or not digits). 
+    """         
+    if input_string == "q":
+        return "quit"
+
+    split = input_string.split()
+        
+    if (len(split) < 2 or
+       len(split) > 3 or 
+       split[0] not in operators):
+        print "Error! Please provide an operator and one or two operands."
+        return False
+
+    tokens = [split[0]]
+
     for past_one in split[1:]:
         try:
             tokens.append(int(float(past_one)))
         except ValueError:
-            print "Please use numbers"
+            print "Error! Please use numbers."
             return False
     return tokens
 
 
 def main():
-    '''takes raw_input (str); returns the answer'''
-    computing = True
-    while computing == True:
-        comp = tokenize(raw_input("> "))
-        if comp == False:
-            print "Try again or type q"
-        elif comp[0] == "q":
-            computing = False
-        else:
-            answer = calculate(comp)
-            print answer
+    """ Initiate calculator and compute input.
+
+    Program will call instructions, then tokenize, then calculate. 
+    """
+    instructions()
+    while True:
+        # Both tokenize and calculate can return False if user input
+        # is incalculable. The assigned tokenized_input will then be False.
+        tokenized_input = tokenize(raw_input("> "))
+        if tokenized_input == "quit":
+            break 
+        if tokenized_input:
+            tokenized_input = calculate(tokenized_input)   
+        if tokenized_input == False:  
+            instructions()
 
 
 if __name__ == '__main__':
